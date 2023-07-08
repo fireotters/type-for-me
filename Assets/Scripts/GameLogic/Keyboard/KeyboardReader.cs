a9a48132720cc7eb895e00503bb73287195f0f49
Rubén Bermejo Romero <mejoruben@gmail.com>
2023-07-08 14:05:46 +0200

    made progress on word prompt logic * now it can go through the entire list * marks as complete completed phrases * triggers level end victory when all phrases are typed in

--- Assets/Scripts/GameLogic/Keyboard/KeyboardReader.cs
+++ Assets/Scripts/GameLogic/Keyboard/KeyboardReader.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Collections;
 using Signals;
 using TMPro;
 using UnityEngine;
@@ -11,6 +13,9 @@ namespace GameLogic.Keyboard
         [SerializeField] private TMP_Text textPreview;
         [SerializeField] private TMP_Text inputtedText;
         private readonly CompositeDisposable disposables = new();
+
+        [SerializeField] private GameObject progressTracker;
+        [SerializeField] private GameObject wordTrackerPrefab;
         
         private void Start()
         {
@@ -23,10 +28,72 @@ namespace GameLogic.Keyboard
             }
             else
             {
+                var startingXAxisPos = progressTracker.transform.position.x;
+                for (var i = 0; i < phrases.Length; i++)
+                {
+                    var newGameObject 
+                        = Instantiate(wordTrackerPrefab, new Vector3(startingXAxisPos, progressTracker.transform.position.y), Quaternion.identity,  progressTracker.transform);
+                    startingXAxisPos -= .5f;
+                    var spriteRenderer = newGameObject.GetComponent<SpriteRenderer>();
+                    spriteRenderer.color = i == phrases.Length - 1 ? Color.white : Color.gray;
+                }
                 textPreview.text = phrases[0];
             }
         }
 
+        private void CheckForCompletedTest()
+        {
+            if (textPreview.text.Equals(inputtedText.text))
+            {
+                Debug.Log("gaming yo yo gaming gaming yo");
+                inputtedText.text = $"<color=#4EFF00>{inputtedText.text}</color>";
+                StartCoroutine(UpdateGameState());
+            }
+            else
+            {
+                Debug.Log("keep goin");
+            }
+        }
+
+        private IEnumerator UpdateGameState()
+        {
+            yield return new WaitForSeconds(2f);
+            var currentWord = textPreview.text;
+            var trackerSpriteRenderers = progressTracker.GetComponentsInChildren<SpriteRenderer>();
+            
+            if (currentWord.Equals(phrases[^1]))
+            {
+                Debug.Log("Turns out you DONT suck!");
+                var lastTrackerRenderer = trackerSpriteRenderers[0];
+                lastTrackerRenderer.color = Color.green;
+                SignalBus<SignalGameEnded>.Fire(new SignalGameEnded { result = GameEndCondition.WinType1, score = 0 });
+            }
+            else
+            {
+                // last word wasn't reached yet, NEEXT!
+                inputtedText.text = string.Empty;
+                var nextWordIndex = phrases.ToList().IndexOf(currentWord) + 1;
+                textPreview.text = phrases[nextWordIndex];
+                
+                // also update hud
+                // the way these are instance we gotta iterate through the list backwards
+                for (var i = trackerSpriteRenderers.Length - 1; i >= 0; i--)
+                {
+                    var sr = trackerSpriteRenderers[i];
+                    if (sr.color.Equals(Color.green))
+                        continue;
+                    if (sr.color.Equals(Color.white))
+                    {
+                        // that's the current one we just beat, mark as valid and the next one as current
+                        sr.color = Color.green;
+                        var nextSpriteRenderer = trackerSpriteRenderers[i - 1];
+                        nextSpriteRenderer.color = Color.white;
+                        break;
+                    }
+                }
+            }
+        }
+        
         private void ReadFromKeyboard(SignalKeyboardKeyPress keyPress)
         {
             Debug.Log($"<KeyboardReader> {keyPress.Letter} was pressed!");
@@ -55,7 +122,8 @@ namespace GameLogic.Keyboard
                 // TODO: perhaps a "bzzt wrong" sound could play here?
                 // should we let the player keep typing???
             }
-            
+
+            CheckForCompletedTest();
         }
 
         private void BackspaceAction(SignalKeyboardBackspacePress backspacePress)
